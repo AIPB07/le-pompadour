613840424f6cb753a329af959a61e52420a2a251
AIPB07 <binyon.aip@gmail.com>
2020-03-02 14:24:41 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,75 @@
+// Import images
+import navbarBrand from '../src/img/el-pompodour-7.png';
+import heroLeft from '../src/img/men-having-their-haircut-left.jpg';
+import heroRight from '../src/img/men-having-their-haircut-right.jpg';
+import serviceHair from '../src/img/person-combing.jpg';
+import serviceBeard from '../src/img/beard.jpg';
+import serviceDiscount from '../src/img/boy-cut.jpg';
+import welcomeLogo from '../src/img/el-pompodour-5_2';
+import pompadourWoman from '../src/img/pompadour-woman';
+import barberPole from '../src/img/barber-pole-gold';
+import goldMotif from '../src/img/gold-motif2';
+
+// Import stylesheets
+import '../src/styles/app';
+
+// Set image src attributes
+
+var navbarBrandImg = document.getElementById('navbarBrand');
+navbarBrandImg.src = navbarBrand;
+
+var heroLeftImg = document.getElementById('heroLeft');
+heroLeftImg.src = heroLeft;
+
+var heroRightImg = document.getElementById('heroRight');
+heroRightImg.src = heroRight;
+
+var heroLogoImg = document.getElementById('heroLogo');
+heroLogoImg.src = welcomeLogo;
+
+var serviceHairImg = document.getElementById('serviceHair');
+serviceHairImg.src = serviceHair;
+
+var serviceBeardImg = document.getElementById('serviceBeard');
+serviceBeardImg.src = serviceBeard;
+
+var serviceDiscountImg = document.getElementById('serviceDiscount');
+serviceDiscountImg.src = serviceDiscount;
+
+var welcomeLogoImg = document.getElementById('welcomeLogo');
+welcomeLogoImg.src = welcomeLogo;
+
+var pompadourWomanImg = document.getElementById('pompadourWoman');
+pompadourWomanImg.src = pompadourWoman;
+
+var hairModalTitleImg = document.getElementById('hairModalTitle');
+hairModalTitleImg.src = welcomeLogo;
+
+var beardModalTitleImg = document.getElementById('beardModalTitle');
+beardModalTitleImg.src = welcomeLogo;
+
+var discountModalTitleImg = document.getElementById('discountModalTitle');
+discountModalTitleImg.src = welcomeLogo;
+
+var hairBarberPoleImg = document.getElementById('hairBarberPole');
+hairBarberPoleImg.src = barberPole;
+
+var beardBarberPoleImg = document.getElementById('beardBarberPole');
+beardBarberPoleImg.src = barberPole;
+
+var discountBarberPoleImg = document.getElementById('discountBarberPole');
+discountBarberPoleImg.src = barberPole;
+
+// var testimonialLogoImg = document.getElementById('testimonialLogo');
+// testimonialLogoImg.src = welcomeLogo;
+
+// var tarporleyLogoImg = document.getElementById('tarporleyLogo');
+// tarporleyLogoImg.src = welcomeLogo;
+
+// var sandbachLogoImg = document.getElementById('sandbachLogo');
+// sandbachLogoImg.src = goldMotif;
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+            test: /\.html$/,
+            use: [
+              {
+                loader: 'html-loader',
+                options: { minimize: !isDevelopment }
+              }
+            ]
+        },
+        {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              'file-loader',
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                    quality: 65
+                  },
+                  optipng: {
+                    enabled: !isDevelopment
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.90],
+                    speed: 4
+                  },
+                  gifsicle: {
+                    interlaced: false
+                  },
+                  webp: {
+                    quality: 75
+                  }
+                }
+              }
+            ]
+        },
+        {
+            test: /\.module\.s(a|c)ss$/,
+            loader: [
+              isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  sourceMap: isDevelopment
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: isDevelopment
+                }
+              }
+            ]
+          },
+          {
+            test: /\.s(a|c)ss$/,
+            exclude: /\.module.(s(a|c)ss)$/,
+            loader: [
+              isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+              'css-loader',
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: isDevelopment
+                }
+              }
+            ]
+          }
+      ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.gif', '.png', '.jpg', '.jpeg', '.svg']
+    },
+    output: {
+        filename: isDevelopment ? '[name].js' : '[name].[hash].js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        })
+    ]
+}
